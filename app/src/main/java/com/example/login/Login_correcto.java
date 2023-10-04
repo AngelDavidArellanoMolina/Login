7b59da2a325da3e463c83f7d671d06f1c8ad5bf6
ÁngelDavidArellanoM <angeldamc11@gmail.com>
2023-10-04 14:11:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/login/Login_correcto.java
@@ -0,0 +1,18 @@
+package com.example.login;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login_correcto extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_correcto);
+        TextView texto = findViewById(R.id.result_set);
+
+        String user = getIntent().getStringExtra("USER");
+
+        texto.setText("Bienvenido: " + user);
+    }
+}
---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    String comprobacion = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String correctUser = "david";
+        String correctPass = "pass123";
+
+        EditText User = findViewById(R.id.usr);
+        EditText Password = findViewById(R.id.pswd);
+        Button button = findViewById(R.id.button);
+        TextView texto = findViewById(R.id.error);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usuario = User.getText().toString();
+                String contrasena = Password.getText().toString();
+                Log.d("User: ", usuario);
+                Log.d("User: ", contrasena);
+
+                if (usuario.equals(correctUser) && contrasena.equals(correctPass)) {
+                    setContentView(R.layout.login_correcto);
+                    texto.setText("");
+                    comprobacion = usuario;
+                    comporbacion(button);
+                } else {
+                    comprobacion = "¡Login incorrecto!";
+                    texto.setText(comprobacion);
+                }
+            }
+        });
+
+    }
+    public void comporbacion (View view){
+        Intent i = new Intent(this, Login_correcto.class);
+        i.putExtra("USER", comprobacion);
+        startActivity(i);
+
+    }
+}
